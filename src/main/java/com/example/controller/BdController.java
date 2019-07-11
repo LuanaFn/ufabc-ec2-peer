2fd9bd4fb0bde61c41a81533fd8d6512c4f5ad1c
Luana Nascimento <lluana13@hotmail.com>
2019-07-11 17:14:02 -0300

    dependencia h2 runtime

--- src/main/java/com/example/controller/BdController.java
+++ src/main/java/com/example/controller/BdController.java
@@ -2,10 +2,7 @@ package com.example.controller;
 
 import java.util.Map;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,11 +19,4 @@ public class BdController {
 	String db(Map<String, Object> model) {
 		return bo.testaBd(model);
 	}
-	
-	@Bean
-    ServletRegistrationBean h2servletRegistration(){
-        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-        registrationBean.addUrlMappings("/h2/*");
-        return registrationBean;
-    }
 }
