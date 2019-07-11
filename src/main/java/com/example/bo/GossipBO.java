0ca4f5b41826d1ff9d204298987540d784d02098
Luana Nascimento <lluana13@hotmail.com>
2019-07-11 18:03:44 -0300

    agendamento acontecendo

--- src/main/java/com/example/bo/GossipBO.java
+++ src/main/java/com/example/bo/GossipBO.java
@@ -7,26 +7,32 @@ import javax.annotation.PostConstruct;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.example.dto.Estado;
 import com.example.repository.EstadoRepository;
 
+@EnableScheduling
 @EnableJpaRepositories("com.example.repository")
 @Component
 public class GossipBO {
-	
+
 	@Autowired
 	EstadoRepository repo;
-	
+
+	private final long SEGUNDO = 1000;
+	private final long MINUTO = SEGUNDO * 60;
+	private final long HORA = MINUTO * 60;
+
 	@Autowired
 	public GossipBO() {
 
 	}
 
 	/**
-	 * Define um estado inicial para a máquina caso
-	 * ainda não tenha nenhum
+	 * Define um estado inicial para a máquina caso ainda não tenha nenhum
 	 * 
 	 * @param repo
 	 */
@@ -39,7 +45,7 @@ public class GossipBO {
 			estado.setTime(new Date());
 
 			System.out.println(repo.save(estado).toString());
-			
+
 			repo.flush();
 		}
 	}
@@ -47,13 +53,14 @@ public class GossipBO {
 	/**
 	 * Gera uma nova palavra de 10 digitos e seta como estado
 	 */
-	public void startGossip(EstadoRepository repo) {
+	@Scheduled(fixedDelay = SEGUNDO*3)
+	public void startGossip() {
 		String palavra = RandomStringUtils.randomAlphabetic(10);
-		
+
 		Estado estado = repo.findById(1l).get();
 		estado.setMensagem(palavra);
 		estado.setTime(new Date());
-		
+
 		repo.save(estado);
 	}
 }
